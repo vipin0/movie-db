ee3034ddfc7f4abcbb95fb1645a4e7d294888d48
vipin0 <vipin6673@gmail.com>
2021-07-26 22:14:14 +0530

    added permissions and genric views

--- movieDB/urls.py
+++ movieDB/urls.py
@@ -22,6 +22,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
+    
     # for openapi
     path('api-auth/',include('rest_framework.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
