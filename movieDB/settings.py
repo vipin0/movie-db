8bdae027fa741345709158c62e76e6676fbce761
vipin0 <vipin6673@gmail.com>
2021-07-27 13:52:19 +0530

    changed serialiers

--- movieDB/settings.py
+++ movieDB/settings.py
@@ -136,7 +136,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # APPEND_SLASH=False
--- movieDbApp/serializers.py
+++ movieDbApp/serializers.py
@@ -1,5 +1,6 @@
-from movieDbApp.models import Movie, Review,StreamingPlatform
+from django.core.paginator import Paginator
 from rest_framework import serializers
+from movieDbApp.models import Movie, Review,StreamingPlatform
 
 class ReviewReadSerializer(serializers.ModelSerializer):
     review_user = serializers.StringRelatedField(read_only=True)
@@ -8,11 +9,6 @@ class ReviewReadSerializer(serializers.ModelSerializer):
         model = Review
         fields = '__all__'
 
-class ReviewSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Review
-        fields = '__all__'
 
 class ReviewWriteSerializer(serializers.ModelSerializer):
     review_user = serializers.StringRelatedField(read_only=True)
@@ -22,30 +18,75 @@ class ReviewWriteSerializer(serializers.ModelSerializer):
         model =  Review
         # fields = ['rating','description','active']
         fields = '__all__'
-        read_only_fields  = ['id','review_user','created','modified','movie',]
+        read_only_fields  = ['id','review_user','created','modified','movie','active']
 
 class MovieSerializer(serializers.ModelSerializer):
-    reviews = ReviewReadSerializer(many=True,read_only=True)
+    # reviews = ReviewReadSerializer(many=True,read_only=True)
+    reviews = serializers.StringRelatedField(many=True,read_only=True)
+    # reviews = serializers.SerializerMethodField('paginated_reviews')
     platform = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
         read_only_fields = ['id','released_on','average_rating','number_rating']
 
+    def paginated_reviews(self, obj):
+        try:
+            page_size = self.context.get('request').query_params.get('size')
+        except Exception as e:
+            page_size = 10
+        if page_size is None:
+            page_size = 10
+        # page_size = self.context.get('request').query_params.get('size') or 10
+        paginator = Paginator(obj.movie.all(), page_size)
+        try:
+            page = self.context.get('request').query_params.get('page')
+        except Exception as e:
+            page = 1
+        # page = self.context.get('request').query_params.get('page') or 1
+        if page is None:
+            page = 1
+        review = paginator.page(page)
+        serializer = MovieSerializer(review, many=True)
+        return serializer.data
+
+    
+
 class StreamingPlatformSerializer(serializers.ModelSerializer):
 
-    movies = MovieSerializer(many=True,read_only=True) # gives everything
-    
-    # movies = serializers.StringRelatedField(many=True)   # returns only name
+    # movies = MovieSerializer(many=True,read_only=True) # gives everything
 
-    # movies = serializers.HyperlinkedRelatedField(
-    #     many=True,
-    #     read_only=True,
-    #     view_name='movie_detail'
-    # )
+    # movies = serializers.SerializerMethodField('paginated_movies')
+    
+    # movies = serializers.StringRelatedField(many=True,read_only=True)   # returns only name
+
+    movies = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='movie_detail'
+    )
     
     class Meta:
         model = StreamingPlatform
+        fields = '__all__'
+
+    def paginated_movies(self, obj):
+        page_size = self.context['request'].query_params.get('size') or 10
+        paginator = Paginator(obj.movies.all(), page_size)
+        page = self.context['request'].query_params.get('page') or 1
+
+        movie = paginator.page(page)
+        serializer = MovieSerializer(movie, many=True)
+        return serializer.data
+
+
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
         fields = '__all__'
 
 
