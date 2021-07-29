731a3a84f78d6b384a9e51e783b5549d031fa455
vipin0 <vipin6673@gmail.com>
2021-07-29 09:26:12 +0530

    added tests for Actors

--- movieDbApp/tests.py
+++ movieDbApp/tests.py
@@ -59,6 +59,9 @@ class MovieTestCase(APITestCase):
         self.token = Token.objects.get(user__username=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
+        actors = [models.Star(full_name='Test1',about="Test1"),models.Star(full_name='Test2',about="Test2"),]
+
+        self.actors = models.Star.objects.bulk_create(actors)
         self.stream = models.StreamingPlatform.objects.create(
                                                                 name="netflix",
                                                                 about="netflix",
@@ -70,12 +73,15 @@ class MovieTestCase(APITestCase):
                                                 active= True,
                                                 platform=self.stream
                                             )
+        # self.movie.stars.set(self.actors)
+    
     def test_movie_create(self):
         data = {
                 "name": "TestMovie",
                 "description": "TEst --updated",
                 "active": True,
-                "platform": self.stream.id
+                "platform": self.stream.id,
+                "stars": ''
             }
         response = self.client.post(reverse('movie_list'),data)
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
@@ -103,7 +109,7 @@ class MovieTestCase(APITestCase):
         self.assertEqual(response.status_code,status.HTTP_403_FORBIDDEN)
 
 
-class MovieTestCase(APITestCase):
+class ReviewTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="test",password="Test@1234")
         self.user1 = User.objects.create_user(username="test1",password="Test@1234")
@@ -176,3 +182,44 @@ class MovieTestCase(APITestCase):
     def test_review_delete(self):
         response = self.client.delete(reverse('review_detail',args=(self.movie.id,self.review.id,)))
         self.assertEqual(response.status_code,status.HTTP_204_NO_CONTENT)
+
+
+class ActorTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="test",password="Test@1234")
+        
+        self.token = Token.objects.get(user__username=self.user)
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.actor = models.Star.objects.create(full_name='Test Actor',about='This is a test actor description.')
+
+    def test_actor_create(self):
+        data = {
+                "full_name": "Test",
+                "about":"test"
+            }
+
+        response = self.client.post(reverse('actor_list'),data)
+        self.assertEqual(response.status_code,status.HTTP_403_FORBIDDEN)
+    
+    def test_review_list(self):
+        response = self.client.get(reverse('actor_list'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_review_get_one(self):
+        response = self.client.get(reverse('actor_detail',args=(self.actor.id,)))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_review_put(self):
+        data = {
+                "full_name": "Test",
+                "about":"test"
+            }
+
+        response = self.client.put(reverse('actor_detail',args=(self.actor.id,)),data)
+        self.assertEqual(response.status_code,status.HTTP_403_FORBIDDEN)
+    
+    def test_review_delete(self):
+        response = self.client.delete(reverse('actor_detail',args=(self.actor.id,)))
+        self.assertEqual(response.status_code,status.HTTP_403_FORBIDDEN)
