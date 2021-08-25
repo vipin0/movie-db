1b2203d0f2d94f687d47e28c676d6779e7a2eb5f
vipin0 <vipin6673@gmail.com>
2021-08-25 17:17:06 +0530

    static files changed

--- movieDB/urls.py
+++ movieDB/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import debug_toolbar
 
@@ -37,6 +37,7 @@ urlpatterns = [
 # for serving static files 
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
    urlpatterns+=[
