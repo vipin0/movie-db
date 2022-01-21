9b3947e4847b3305dd18b6d44ffcabab16c56476
vipin0 <vipin6673@gmail.com>
2022-01-21 21:10:25 +0530

    Update ugettext_lazy to gettext_lazy in django4

--- movieDbApp/models.py
+++ movieDbApp/models.py
@@ -1,7 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
 # Create your models here.
